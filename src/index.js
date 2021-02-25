a5d822d59a7112195c9667511e352ba97b3ac59d
sanggnguyenn <sang.nguyenngoc@gforces.auto>
2021-02-25 18:06:14 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+class MyLibrary {
+  constructor() {
+    console.log("Library constructor loaded");
+  }
+
+  myMethod = () => {
+    console.log("My name is Sang");
+  };
+}
+
+export default MyLibrary;
